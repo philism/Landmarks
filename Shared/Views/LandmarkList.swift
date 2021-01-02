98f25da87d7c953d456c44191269583fa5a24210
Phil Smith <philstech@icloud.com>
2021-01-02 15:36:36 -0800

    Finished Building Lists and Navigation

---
+++ Shared/Views/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Philip Smith on 1/2/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd Generation)", "iPhone 12 Pro Max", "iPad Pro (9.7-inch)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
