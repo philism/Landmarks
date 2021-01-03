67f2785baa49bd304363fd6ed709c0cad5f88ec9
Phil Smith <philstech@icloud.com>
2021-01-03 15:39:27 -0800

    Completed Handling User Inputs

---
+++ Shared/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Philip Smith on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
