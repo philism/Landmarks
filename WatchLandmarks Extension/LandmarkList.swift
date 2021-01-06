63e6b82e2dc0afea4dd1b9ea26ca1e9ffd2bae1c
Phil Smith <philstech@icloud.com>
2021-01-05 21:26:36 -0800

    Completed Creating a Mac OS App

---
+++ WatchLandmarks Extension/LandmarkList.swift
@@ -0,0 +1,49 @@
+//
+//  LandmarkList.swift
+//  WatchLandmarks Extension
+//
+//  Created by Philip Smith on 1/5/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
+            }
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+        
+        
+//        ForEach(["iPhone SE (2nd Generation)", "iPhone 12 Pro Max", "iPad Pro (9.7-inch)"], id: \.self) { deviceName in
+//            LandmarkList()
+//                .environmentObject(ModelData())
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+    }
+}
